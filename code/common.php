079044a33191de2f7694a68f418248e1f221ac97
cuizhongyi <zhongyi.cui@ioa365.com>
2020-05-15 11:41:53 +0800

    'function2'

--- code/common.php
+++ code/common.php
@@ -318,3 +318,304 @@ function get_short_url($url = '')
     $short_url = file_get_contents($api_url . $url);
     return $short_url;
 }
+
+// **********转换类***************
+
+/**
+ * 将xml格式转换为数组
+ * @param string $xml xml字符串
+ * @return mixed
+ */
+function xml_to_array($xml = '')
+{
+    // 利用函数simplexml_load_string()把xml字符串载入对象中
+    $obj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+    // 编码对象后，再解码即可得到数组
+    $arr = json_decode(json_encode($obj), true);
+    return $arr;
+}
+
+/**
+ * 隐藏手机号中间四位数为****
+ * @param string $mobile 正常手机号
+ * @return mixed
+ */
+function replace_phone($mobile = '')
+{
+    $new_mobile = substr_replace($mobile, '****', 3, 4);
+    return $new_mobile;
+}
+
+/**
+ * 最简单的Ajax请求返回数据格式
+ * @param string $msg 返回提示信息
+ * @param int $code 返回标识符号
+ * @param array $data 返回数据
+ */
+function ajax_return($msg = '', $code = 0, $data = [])
+{
+    $return['code'] = $code;
+    $return['msg'] = $msg;
+    $return['data'] = $data;
+    exit(json_encode($return, JSON_UNESCAPED_UNICODE));
+}
+
+/**
+ * 截取字符串，超出部分用省略符号显示
+ * @param string $text 待截取字符串
+ * @param int $length 截取长度，默认全部截取
+ * @param string $rep 截取超出替换的字符串，默认为省略号
+ * @return string
+ */
+function cut_string($text = '', $length = 0, $rep = '…')
+{
+    if (!empty($length) && mb_strlen($text, 'utf8') > $length) {
+        $text = mb_substr($text, 0, $length, 'utf8') . $rep;
+    }
+    return $text;
+}
+
+/**
+ * CURL请求之GET方式
+ * @param string $url 请求接口地址
+ * @return bool|mixed
+ */
+function curl_get($url = '')
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // 不验证SSL证书。
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+    return $res;
+}
+
+/**
+ * CURL请求之POST方式
+ * @param string $url 请求接口地址
+ * @param array $data 请求参数
+ * @param int $timeout 超时时间
+ * @return mixed
+ */
+function curl_post($url = '', $data = [], $timeout = 3000)
+{
+    $post_data = http_build_query($data, '', '&');
+    header("Content-type:text/html;charset=utf-8");
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+    return $res;
+}
+
+/**
+ * 根据生日计算年龄
+ * @param string $date 生日的年月日
+ * @return int
+ */
+function get_age($date = '')
+{
+    $age = 0;
+    $time = strtotime($date);
+    // 日期非法，则不处理
+    if (!$time) {
+        return $age;
+    }
+    // 计算时间年月日差
+    $date = date('Y-m-d', $time);
+    list($year, $month, $day) = explode("-", $date);
+    $age = date("Y", time()) - $year;
+    $diff_month = date("m") - $month;
+    $diff_day = date("d") - $day;
+    // 不满周岁年龄减1
+    if ($age < 0 || $diff_month < 0 || $diff_day < 0) {
+        $age--;
+    }
+    return $age;
+}
+
+/**
+ * 获取毫秒数
+ * @return string
+ */
+function get_millisecond()
+{
+    list($t1, $t2) = explode(' ', microtime());
+    $ms = sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);
+    return $ms;
+}
+
+/**
+ * 日期时间显示格式转换
+ * @param int $time 时间戳
+ * @return bool|string
+ */
+function transfer_show_time($time = 0)
+{
+    // 时间显示格式
+    $day_time = date("m-d H:i", $time);
+    $hour_time = date("H:i", $time);
+    // 时间差
+    $diff_time = time() - $time;
+    $date = $day_time;
+    if ($diff_time < 60) {
+        $date = '刚刚';
+    } else if ($diff_time < 60 * 60) {
+        $min = floor($diff_time / 60);
+        $date = $min . '分钟前';
+    } else if ($diff_time < 60 * 60 * 24) {
+        $h = floor($diff_time / (60 * 60));
+        $date = $h . '小时前 ' . $hour_time;
+    } else if ($diff_time < 60 * 60 * 24 * 3) {
+        $day = floor($diff_time / (60 * 60 * 24));
+        if ($day == 1) {
+            $date = '昨天 ' . $day_time;
+        } else {
+            $date = '前天 ' . $day_time;
+        }
+    }
+    return $date;
+}
+
+// ***********验证类
+
+/**
+ * 校验是否为合法格式的手机号
+ * @param string $mobile 手机号码
+ * @return bool
+ */
+function check_mobile($mobile = '')
+{
+    // 非数字直接false
+    if (!is_numeric($mobile)) {
+        return false;
+    }
+    $pattern = '#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,3,7,8]{1}\d{8}$|^18[\d]{9}$|^19[9]{1}\d{8}$#';
+    $res = preg_match($pattern, $mobile) ? true : false;
+    return $res;
+}
+
+/**
+ * 校验是否为合法格式的邮箱
+ * @param string $email 邮箱
+ * @return bool
+ */
+function check_email($email = '')
+{
+    $pattern = '/([\w\-]+\@[\w\-]+\.[\w\-]+)/';
+    $res = preg_match($pattern, $email) ? true : false;
+    return $res;
+}
+
+/**
+ * 检查是否为合法格式的日期
+ * @param string $date 日期
+ * @param string $sep 分隔符，默认为横线-
+ * @return bool
+ */
+function check_date($date = '', $sep = '-')
+{
+    $date_arr = explode($sep, $date);
+    $res = false;
+    // 校验日期是否为合法数字
+    if (count($date_arr) == 3 && is_numeric($date_arr[0]) && is_numeric($date_arr[1]) && is_numeric($date_arr[2])) {
+        $res = checkdate($date_arr[1], $date_arr[2], $date_arr[0]);
+    }
+    return $res;
+}
+
+/**
+ * 检查是否为合法格式的时间
+ * @param string $time 时分秒时间
+ * @param string $sep 分隔符，默认为冒号:
+ * @return bool
+ */
+function check_time($time = '', $sep = ":")
+{
+    $time_arr = explode($sep, $time);
+    $res = false;
+    // 校验时间的时分秒是否在合理范围内
+    if (count($time_arr) == 3 && is_numeric($time_arr[0]) && is_numeric($time_arr[1]) && is_numeric($time_arr[2])) {
+        if (($time_arr[0] >= 0 && $time_arr[0] <= 23) && ($time_arr[1] >= 0 && $time_arr[1] <= 59) && ($time_arr[2] >= 0 && $time_arr[2] <= 59)) {
+            $res = true;
+        }
+    }
+    return $res;
+}
+
+/**
+ * 获取指定长度的随机字符串
+ * @param int $length 随机字符串的长度
+ * @return string
+ */
+function get_random_str($length = 9)
+{
+    // 字符串集合，全部大小写字母和数字
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $str = '';
+    // 循环多次，随机逐次获取
+    for ($i = 0; $i < $length; $i++) {
+        $str .= $chars[mt_rand(0, strlen($chars) - 1)];
+    }
+    return $str;
+}
+
+/**
+ * 统计文章中文字和图片的数量
+ * @param string $html 文章html字符串
+ * @return array
+ */
+function count_word_img($html = '')
+{
+    // 匹配img标签的数量
+    preg_match_all('/<img /i', $html, $match_arr);
+    $img_count = count($match_arr[0]);
+    // 统计非img标签的数量，可根据实际情况进行调整表达式
+    $pattern = '/\[#img_[0-9]+_[a-z]*_[0-9]+_[a-zA-Z]*/i';
+    preg_match_all($pattern, $html, $match_arr);
+    $img_count += count($match_arr[0]);
+    // 去掉图片img标签
+    $html = preg_replace("/<img([^>].+)>/iU", "", $html);
+    // 去掉非标签的图片
+    $html = preg_replace($pattern, "", $html);
+    // 去掉全部空格
+    $html = str_replace(' ', '', $html);
+    // 先去除HTML和PHP标记，再统计字数
+    $word_count = mb_strlen(trim(strip_tags($html)), 'UTF-8');
+    return ['word_count' => $word_count, 'img_count' => $img_count];
+}
+
+/**
+ * 格式化打印输出内容
+ * @param void $data 需要打印的内容
+ * @param bool $exit
+ */
+function dump_plus($data, $exit = true)
+{
+    // 自定义样式，方便美观查看
+    $output = '<pre style="display: block;background-color: #f5f5f5;border: 1px solid #cccccc;padding: 10px;margin: 45px 0 0 0;font-size: 13px;line-height: 1.5;border-radius: 4px;">';
+    // boolean或者null类型直接文字输出，其他print_r格式化输出
+    if (is_bool($data)) {
+        $show = $data ? 'true' : 'false';
+    } else if (is_null($data)) {
+        $show = 'null';
+    } else {
+        $show = var_export($data, true);
+    }
+    // 拼接文本输出
+    $output .= $show;
+    $output .= '</pre>';
+    echo $output;
+    // 是否中断执行
+    if ($exit) {
+        exit();
+    }
+}
